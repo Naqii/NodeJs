db7a57c9d1c6e8bc421670e83bde9c1d0f3b1f46
Naqi <sunanhaqi.sh@gmail.com>
2023-04-03 09:14:23 +0700

    initial commit

---
+++ filesystem/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const fileReadCallback = (error, data) => {
+    if (error) {
+        console.log('Gagal membaca berkas')
+        return;
+    }
+    console.log(data)
+};
+
+fs.readFile('notes.txt', 'UTF-8', fileReadCallback);
